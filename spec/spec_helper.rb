89f129add2aa83048ba873c68ebbbd244bdc511b
Vladimir Dementyev <palkan@hey.com>
2024-04-19 18:30:10 -0700

    + ruby on whales

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ unless ENV['DISABLE_SIMPLECOV'] == 'true'
 end
 
 RSpec.configure do |config|
-  config.example_status_persistence_file_path = 'tmp/rspec/examples.txt'
+  config.example_status_persistence_file_path = 'tmp/cache/rspec/examples.txt'
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
